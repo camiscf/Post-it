5aa1388bd228ac193492b24ca4219db95d98ef6a
Camila <87675978+camiscf@users.noreply.github.com>
2022-03-14 17:18:39 -0300

    Add files via upload

---
+++ src/components/CardNota/CardNota.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./estilo.css";
+class CardNota extends Component {
+  render() {
+    return (
+      <section className="card-nota">
+        <header className="card-nota_cabecalho">
+          <h3 className="card-nota_titulo">Título</h3>
+        </header>
+        <p className="card-nota_texto">Escreva sua nota</p>
+      </section>
+    );
+  }
+}
+
+export default CardNota;
